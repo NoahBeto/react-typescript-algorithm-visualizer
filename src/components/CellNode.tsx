8406fc8c4ebf2d22253b18c814b9ff4fef656cc8
JJeshua <79337598+JJeshua@users.noreply.github.com>
2024-01-05 21:25:30 -0800

    Moved CellNode styles to dedicated css file

--- src/components/CellNode.tsx
+++ src/components/CellNode.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CellStyles } from "../ts/cell";
+import "./CellNode.css";
 
 export const CellNode = ({ cellStyles }: { cellStyles: CellStyles }) => {
   const styles = `cell ${cellStyles}`;
